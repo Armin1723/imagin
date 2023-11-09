4085777400b047a68799f8db49bb5450767993e8
Armin1723 <110764802+Armin1723@users.noreply.github.com>
2023-11-09 19:13:21 +0530

    testing build

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -23,7 +23,7 @@ const Login = ({setIsLoggedIn}) => {
 
   const handleLogin = async (e)=>{
     e.preventDefault()
-      const response = await fetch('http://localhost:8000/api/user/login',{headers : {
+      const response = await fetch('https://imagin-backend.onrender.com/api/user/login',{headers : {
         'Content-Type' : 'application/json',
         },method : 'post' ,
         body : JSON.stringify({
